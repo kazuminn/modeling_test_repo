362b0ded48b94e29bc54bfd495e1c522ecc3b238
kazuminn <warugaki_kazumi@docomo.ne.jp>
2015-10-17 22:31:41 +0900

    add SecondView.swift

---
+++ modeling_test_2/SecondView.swift
@@ -0,0 +1,21 @@
+//
+//  SecondView.swift
+//  modeling_test_2
+//
+//  Created by 木下　和巳 on 10/17/15.
+//  Copyright © 2015 test. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Secondview: UIViewController {
+    
+    override func viewDidLoad(){
+        
+        
+    
+    }
+    
+    
+}
